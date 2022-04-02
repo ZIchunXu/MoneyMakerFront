a99ec8d8b6438037ce27f6c0f073ac14b7161e6c
ZIchunXu <wudao.lilian@gmail.com>
2022-04-02 16:26:22 -0700

    2

--- Frontend/src/commom/constants.js
+++ Frontend/src/commom/constants.js
@@ -2,6 +2,8 @@ let constants = {
     currenciesUrl: "api/currencies",
     convertUrl: "api/convert?from=&to=",
     chartUrl:"api/chart?from=&to=",
+    alertUrl:"api/alert",
+    portfolioUrl:"api/portfolio",
 };
 
 export default constants;
--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -2,11 +2,13 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import axios from "axios";
 import router from './router'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import locale from 'element-ui/lib/locale/lang/en'
 import DrVueEcharts from 'dr-vue-echarts';
+Vue.prototype.$axios = axios;
 Vue.use(DrVueEcharts)
 Vue.use(ElementUI, { locale })
 Vue.use(ElementUI);
