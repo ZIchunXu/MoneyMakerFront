04e73fbb5060bbb582f93fbfa79f6960a09697d1
ZIchunXu <zxu72@my.bcit.ca>
2022-04-05 14:16:02 -0700

    fixed home selector

--- Frontend/src/components/Login/Index.vue
+++ Frontend/src/components/Login/Index.vue
@@ -124,6 +124,7 @@ export default {
         }
       });
     },
+  }, mounted() {
   },
 };
 </script>
@@ -139,7 +140,6 @@ ul {
   padding: 0;
 }
 li {
-  display: inline-block;
   margin: 0 10px;
 }
 a {
